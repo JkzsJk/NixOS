613f994cb27f288037cc261236b22c89756babca
JkzsJk <jasonkhorzs@outlook.com>
2026-02-09 23:29:14 +0800

    Trying out Flake & disable blueman!

--- hosts/dellXps15-9530/configuration.nix
+++ hosts/dellXps15-9530/configuration.nix
@@ -31,8 +31,8 @@
 
   # Enable Bluetooth
   hardware.bluetooth.enable   = true;
+  # services.blueman.enable     = true;
   # hardware.pulseaudio.enable  = true;
-  services.blueman.enable     = true;
 
   # Set your time zone.
   time.timeZone = "Asia/Kuala_Lumpur";
@@ -100,29 +100,44 @@
     ];
   };
 
-  # Host-specific packages (common packages are in modules/common.nix)
+  # Install Firefox.
+  programs.firefox.enable = true;
+
+  # Install Vivaldi.
+  ## The option `programs.vivaldi' does not exist.
+  ### programs.vivaldi.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    # Add machine-specific packages here
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    git
+    vivaldi
+    vscode
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false; # Default was false.
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -131,5 +146,6 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
-
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = false;
 }
