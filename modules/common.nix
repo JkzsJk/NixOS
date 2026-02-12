5946fa040fc09e1b0574c92263903c588ad79da6
--global <jasonkhorzs@outlook.com>
2026-02-12 11:52:41 +0800

    Configure system-wide defaults: zoxide/fzf, Zsh shell, Vivaldi browser, Warp terminal, flakes support, and power management.

--- modules/common.nix
+++ modules/common.nix
@@ -5,6 +5,25 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # Enable experimental features (flakes and nix-command)
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Set Zsh as default shell for all users
+  users.defaultUserShell = pkgs.zsh;
+
+  # Set Vivaldi as default browser
+  environment.sessionVariables.BROWSER = "vivaldi";
+  xdg.mime.defaultApplications = {
+    "text/html" = "vivaldi-stable.desktop";
+    "x-scheme-handler/http" = "vivaldi-stable.desktop";
+    "x-scheme-handler/https" = "vivaldi-stable.desktop";
+    "x-scheme-handler/about" = "vivaldi-stable.desktop";
+    "x-scheme-handler/unknown" = "vivaldi-stable.desktop";
+  };
+  
+  # Set Warp as default terminal
+  environment.sessionVariables.TERMINAL = "warp-terminal";
+
   # Common packages for all machines
   environment.systemPackages = with pkgs; [
     vim
@@ -20,6 +39,7 @@
     zoxide
     fzf
     home-manager
+    pciutils  # lspci for hardware info
   ];
 
   # Configure zoxide and fzf for all shells
