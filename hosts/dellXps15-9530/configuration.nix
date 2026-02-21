1520cf2eff600b31130cb937e8eb0c23c50ce6b3
JkzsJk <jasonkhorzs@outlook.com>
2026-02-21 10:18:32 +0800

    Change scrolling style

--- hosts/dellXps15-9530/configuration.nix
+++ hosts/dellXps15-9530/configuration.nix
@@ -195,7 +195,7 @@
     enable = true;
     touchpad = {
       tapping = true;
-      naturalScrolling = true;
+      naturalScrolling = false; # False = Windows-style scrolling, True = macOS-style scrolling
       disableWhileTyping = true;
       accelSpeed = "0.5";
     };
