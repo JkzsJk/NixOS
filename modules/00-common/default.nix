b6e01799803b4bcc5ba76a88b3db5f6a8034b5cb
JkzsJk <jasonkhorzs@outlook.com>
2026-03-20 20:47:11 +0800

    Remove user from common module & rename files.

--- modules/00-common/default.nix
+++ modules/00-common/default.nix
@@ -6,9 +6,8 @@
     ./01-packages.nix
     ./02-shell.nix
     ./03-system.nix
-    ./04-users.nix
-    ./05-vivaldi.nix
-    ./06-rquickshare.nix
-    ./07-localsend.nix
+    ./04-vivaldi.nix
+    ./05-rquickshare.nix
+    ./06-localsend.nix
   ];
 }
